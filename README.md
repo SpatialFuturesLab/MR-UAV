a30e83f9ed03c12e48f2c0dd18d75d4c429dddc3
SpatialFuturesLab <59878450+SpatialFuturesLab@users.noreply.github.com>
2020-01-14 09:58:06 -0500

    Public Release

--- README.md
+++ README.md
@@ -1,1 +1,156 @@
-# SFL-IAAC
+# Spatial Futures Lab Bebop Autonomy
+
+
+Software for the Bebop Autonomy and integration with Grasshopper.
+
+## Setup Instructions
+
+### Contents
+1. [Install Prerequisites](#1-install-prerequisites)
+2. [Clone repository](#2-clone-or-fork-repositories)
+3. [Compilation/Running](#3-compilation-running)
+4. [Communication](#3-communication)
+
+### 1. Install Prerequisites
+1. __Install [Ubuntu 16.04 64-bit](http://www.ubuntu.com)__
+   
+   _Tutorials_
+
+   The following tutorials could be followed to install Ubuntu from USB.
+   
+   * https://tutorials.ubuntu.com/tutorial/tutorial-create-a-usb-stick-on-windows#0
+   * https://tutorials.ubuntu.com/tutorial/tutorial-install-ubuntu-desktop#0
+   
+2. __Install(http://wiki.ros.org/kinetic/Installation/Ubuntu) ros-kinetic-desktop-full__
+3. __Install Git__
+
+	```sudo apt-get install git```
+4. __Install Required Packages__
+
+	```sudo apt-get install build-essential python-rosdep python-catkin-tools python-pip``` 
+	
+	```sudo apt-get install ros-kinetic-vrpn-client-ros```
+	
+	```sudo apt-get install ros-kinetic-mavros ros-kinetic-mavros-extras```
+	```wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh```
+	```chmod +x install_geographiclib_datasets.sh```
+	```sudo ./install_geographiclib_datasets.sh```
+	
+	```sudo apt-get install ros-kinetic-rosbridge-server```
+
+
+### 2. Clone or Fork Repositories
+1. __Create catkin_workspace__
+
+	```mkdir -p ~/bebop_ws/src && cd~/bebop_ws``` 
+
+2. __Initialize workspace__
+
+	```catkin init```
+
+3. __Clone bebop_autonomy__ 
+
+	```git clone https://github.com/AutonomyLab/bebop_autonomy.git src/bebop_autonomy```
+
+4. __Clone drone_mocap__
+
+	```git clone https://github.com/OsloMet1811/Digital-Twin.git src/drone_mocap```
+
+5. __Clone vrpn_client_ros_kinetic__
+
+	```git clone https://github.com/ros-drivers/vrpn_client_ros.git src/vrpn_client_ros```
+
+6. __Clone SFL_Bebop workspace__
+
+	```git clone https://github.com/SpatialFuturesLab/MR-UAV.git```
+
+
+### 3. Compilation & Running
+1. __Update rosdep and install dependencies__
+
+	```sudo rosdep update```
+	
+	```sudo rosdep install --rosdistro kinetic --from-paths src -i```
+
+2. __Build the workspace__
+
+	```catkin build```
+3. __Update bashrc__
+
+	```alias bebop = source ~/devel/setup.sh```
+
+	Add the above line of code with the path of the setup.sh in bashrc. Everytime a new terminal is opened, type bebop.
+4. __Launch Drone__
+
+	```bebop```
+
+	```roslaunch bebop_driver bebop_node.launch```
+
+### 4. Communication
+ * https://wiki.teltonika-networks.com/view/Setting_up_a_Static_IP_address_on_a_Ubuntu_16.04_PC
+ * https://www.dummies.com/programming/networking/configuring-network-connections-windows-10/
+ 
+## Operating Procedures
+
+### Contents
+1. [Capture Points from Rigid Bodies](#1-capture-points)
+2. [Autonomous Flight](#2-autonomous-flight)
+3. [Manual Flight](#3-manual-flight)
+4. [Marker Snapshot](#3-marker-snapshot)
+
+### 1. Capture Points from Rigid Bodies
+1. __Define Rigid Body (Windows)__
+   * Create a rigid body in Motive using a minimum of 3 markers
+   * Label it using the naming convention GeomToolX where X is an upper case letter (A-Z) 
+   
+2. __Run Python Script (Ubuntu)__
+   * Run Python script(Ubuntu) capture_rigid_body.py
+   * With the terminal active, press a key X on the keyboard (A - Z) to capture the rigid body GeomToolX corresponding to the pressed key
+   * The position and orientation of this rigid body can be obtained by subscribing to the rostopic /rigid_body/GeomToolX
+   ```bebop```
+   
+   ```rostopic echo /rigid_body/GeomToolX```
+   
+### 2. Autonomous Flight
+1. __Launch Drone__
+
+	```bebop```
+	
+	```roslaunch bebop_tools joy_teleop.launch```
+2. __Run Python Script__
+
+	```bebop```
+	
+	```python bebop_controller.py``` 
+
+3. __Toggle Autonomous Mode__
+	* Press Buttons (TODO) on Joystick to toggle to/back from autonomous mode 
+	* In autonomous mode, drone can navigate to the positions given to it
+	
+4. __Define Waypoints__ 
+	* Publish to rostopic bebop/setpoints to add waypoints 
+	* If drone does not receive any new waypoints, it hovers at previously defined waypoint
+	
+	
+### 3. Manual Flight
+1. __Launch Drone__
+
+	```bebop```
+	
+	```roslaunch bebop_tools joy_teleop.launch```
+
+2. __Joystick Control__
+
+	```bebop```
+	
+	```roslaunch bebop_tools joy_teleop.launch```
+	
+3. __Update Joystick Sensitivity__
+
+	* Update script in bebop_autonomy/bebop_tools/config/xbox360.yaml to change sensitivity of drone commands from joystick
+	* Change scale in lines 13, 18, 23, 28 to 0.5
+### 4. Marker Snapshot
+WIP
+```rostopic pub /bebop/setpoint  geometry_msgs/Pose '{position:  {x: -0.5, y: -0.5, z: 1.0}, orientation: {x: 0.0,y: 0.0,z: 0.0, w: 1.0}}'```
+
+
---
+++ Scripts/capture_rigid_bodies.py
@@ -0,0 +1,230 @@
+import sys, select, tty, termios
+import numpy as np
+import string
+
+import rospy
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist, PoseStamped, Pose, PoseArray
+
+class NonBlockingConsole(object):
+	"""Gets inputs from keyboard
+	"""
+    def __enter__(self):
+        self.old_settings = termios.tcgetattr(sys.stdin)
+        tty.setcbreak(sys.stdin.fileno())
+        return self
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old_settings)
+
+    def get_data(self):
+        try:
+            if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
+                return sys.stdin.read(1)
+        except:
+            return '[CTRL-C]'
+        return False
+
+    def __exit__(self, type, value, traceback):
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old_settings)
+
+class CapturePoints:
+	"""Captures points from Geom Tools
+	"""
+	def __init__(self, list_of_tools = list(string.ascii_uppercase), list_of_points = list(string.digits)):
+		self.capturePoints = True
+		self.geomPoseTool = dict()
+		self.prevGeomToolPoint = dict()
+		self.publishGeomToolPose = dict()
+		rospy.init_node('CaptureGeomToolPoints', anonymous=False)		
+		self.toolsList = list_of_tools
+		
+		self.snaphotPose = dict()
+		self.publishSnapshotPose = dict()
+		self.publishSnapshotPoseArray = rospy.Publisher('Snapshot/captured_pose', PoseArray, queue_size=10)
+		
+		self.pointsList = list_of_points
+		self.poseList = PoseArray()
+		
+		for p in self.pointsList:
+			callback = getattr(self, 'pose_callback_point' + p)
+			rospy.Subscriber('/vrpn_client_node/SnapPoint{0}/pose'.format(p),PoseStamped,callback)
+			self.publishSnapshotPose[p] = rospy.Publisher('SnapPoint{0}/captured_pose'.format(p), PoseStamped, queue_size=10)
+			self.snaphotPose[p] = None
+
+		for tool in self.toolsList:
+			callback = getattr(self, 'pose_callback_geom' + tool)
+			rospy.Subscriber('/vrpn_client_node/GeomTool{0}/pose'.format(tool),PoseStamped,callback)
+			self.publishGeomToolPose[tool] = rospy.Publisher('GeomTool{0}/captured_pose'.format(tool), PoseStamped, queue_size=10)
+			self.prevGeomToolPoint[tool] = None
+			self.geomPoseTool[tool] = None
+
+		self.publishVivePose = rospy.Publisher('vive/captured_pose', PoseStamped, queue_size=10)
+
+	
+	def key_reader(self):
+		with NonBlockingConsole() as nbc:
+			while self.capturePoints:
+				c = nbc.get_data()
+				if c == '\x1b': # Esc
+					self.capturePoints = False
+				if c == '\x20': # Space
+					try:
+						for p in self.pointsList:
+							if self.snaphotPose[p] is not None:
+								self.poseList.poses.append(self.snaphotPose[p].pose)
+						self.publishSnapshotPoseArray.publish(self.poseList)
+						print "Publishing list of poses captured"
+						self.poseList = PoseArray()
+					except:
+						print "Cannot send list of poses!!"
+
+
+				elif c is not False:
+					# print c
+					lower_case_tools_list = [ch.lower() for ch in self.toolsList]
+					if c in lower_case_tools_list:
+						tool = c.upper()
+						try:
+							if tool in self.toolsList:
+								pose = self.geomPoseTool[tool]
+								cur_pt = np.array([pose.pose.position.x, pose.pose.position.y, pose.pose.position.z])
+								if self.prevGeomToolPoint[tool] is not None:
+									dist = np.linalg.norm(cur_pt -  self.prevGeomToolPoint[tool])
+								if self.prevGeomToolPoint[tool] is None or dist > 0.1:
+									self.publishGeomToolPose[tool].publish(pose)
+									self.prevGeomToolPoint[tool] = cur_pt
+									print "Capturing Point from GeomTool{0}".format(tool)
+								else:
+									print "\"Exception: Point too close\" Cannot Capture Point from GeomTool{0}".format(tool)
+							else:
+								print "Invalid Key Press: {0}".tool
+						except Exception as e:
+							# print(c)
+							print "\"Exception: GeomTool{0} not defined\" Cannot Capture Point from GeomTool{0}".format(tool)
+					if c in self.pointsList:
+						try:
+							if str(c) in self.pointsList:
+								pose = self.snaphotPose[str(c)]
+								self.publishSnapshotPose[str(c)].publish(pose)
+								print "Capturing Point from Marker {0}".format(str(c))
+							else:
+								print "Invalid Key Press: {0}".format(c)
+						except Exception as e:
+							print "\"Exception: Marker {0} not defined\" Cannot Capture Point from Marker {0}".format(c)
+					
+
+	def pose_callback_geomA(self, msg):
+		self.geomPoseTool['A'] = msg
+	
+	def pose_callback_geomB(self, msg):
+		self.geomPoseTool['B'] = msg
+	
+	def pose_callback_geomC(self, msg):
+		self.geomPoseTool['C'] = msg
+	
+	def pose_callback_geomD(self, msg):
+		self.geomPoseTool['D'] = msg
+	
+	def pose_callback_geomE(self, msg):
+		self.geomPoseTool['E'] = msg
+	
+	def pose_callback_geomF(self, msg):
+		self.geomPoseTool['F'] = msg
+	
+	def pose_callback_geomG(self, msg):
+		self.geomPoseTool['G'] = msg
+	
+	def pose_callback_geomH(self, msg):
+		self.geomPoseTool['H'] = msg
+	
+	def pose_callback_geomI(self, msg):
+		self.geomPoseTool['I'] = msg
+	
+	def pose_callback_geomJ(self, msg):
+		self.geomPoseTool['J'] = msg
+	
+	def pose_callback_geomK(self, msg):
+		self.geomPoseTool['K'] = msg
+	
+	def pose_callback_geomL(self, msg):
+		self.geomPoseTool['L'] = msg
+	
+	def pose_callback_geomM(self, msg):
+		self.geomPoseTool['M'] = msg
+	
+	def pose_callback_geomN(self, msg):
+		self.geomPoseTool['N'] = msg
+
+	def pose_callback_geomO(self, msg):
+		self.geomPoseTool['O'] = msg
+	
+	def pose_callback_geomP(self, msg):
+		self.geomPoseTool['P'] = msg
+	
+	def pose_callback_geomQ(self, msg):
+		self.geomPoseTool['Q'] = msg
+	
+	def pose_callback_geomR(self, msg):
+		self.geomPoseTool['R'] = msg
+	
+	def pose_callback_geomS(self, msg):
+		self.geomPoseTool['S'] = msg
+	
+	def pose_callback_geomT(self, msg):
+		self.geomPoseTool['T'] = msg
+	
+	def pose_callback_geomU(self, msg):
+		self.geomPoseTool['U'] = msg
+	
+	def pose_callback_geomV(self, msg):
+		self.geomPoseTool['V'] = msg
+
+	def pose_callback_geomW(self, msg):
+		self.geomPoseTool['W'] = msg
+
+	def pose_callback_geomX(self, msg):
+		self.geomPoseTool['X'] = msg
+
+	def pose_callback_geomY(self, msg):
+		self.geomPoseTool['Y'] = msg
+	
+	def pose_callback_geomZ(self, msg):
+		self.geomPoseTool['Z'] = msg
+
+	def pose_callback_point0(self, msg):
+		self.snaphotPose['0'] = msg
+	
+	def pose_callback_point1(self, msg):
+		self.snaphotPose['1'] = msg
+	
+	def pose_callback_point2(self, msg):
+		self.snaphotPose['2'] = msg
+	
+	def pose_callback_point3(self, msg):
+		self.snaphotPose['3'] = msg
+	
+	def pose_callback_point4(self, msg):
+		self.snaphotPose['4'] = msg
+	
+	def pose_callback_point5(self, msg):
+		self.snaphotPose['5'] = msg
+	
+	def pose_callback_point6(self, msg):
+		self.snaphotPose['6'] = msg
+	
+	def pose_callback_point7(self, msg):
+		self.snaphotPose['7'] = msg
+	
+	def pose_callback_point8(self, msg):
+		self.snaphotPose['8'] = msg
+	
+	def pose_callback_point9(self, msg):
+		self.snaphotPose['9'] = msg
+
+	def pose_callback_vive(self, msg):
+		self.publishVivePose(msg)
+
+	
+
+if __name__ == '__main__':
+	capture_geom_tools = CapturePoints()
+	capture_geom_tools.key_reader()
